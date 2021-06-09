bb22145f9a519cf8af0727eb9974f968415d587b
MyktybekOmur <mykty.omurbekov@gmail.com>
2021-06-09 17:38:22 +0300

    first commit

---
+++ src/app/services/food.service.ts
@@ -0,0 +1,64 @@
+import { Injectable } from '@angular/core';
+import { Food } from '../models/food.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class FoodService {
+  getFoods(): Food[] {
+    return [
+      {
+        id: 1,
+        title: 'Kebap',
+        price: 25,
+        image: 'assets/images/foods/Kebap.png',
+        description:
+          'Burda içinde neler olduğu hakkında olacaktır',
+      },
+      {
+        id: 2,
+        title: 'Kahvaltı',
+        price: 15,
+        image: 'assets/images/foods/kahvalti.png',
+        description:
+          'Burda içinde neler olduğu hakkında olacaktır',
+      },
+      {
+        id: 3,
+        title: 'Döner',
+        price: 10,
+        image: 'assets/images/foods/doner.png',
+        description:
+          'Burda içinde neler olduğu hakkında olacaktır',
+      },
+      {
+        id: 4,
+        title: 'Pizza',
+        price: 20,
+        image: 'assets/images/foods/pizza.png',
+        description:
+          'Burda içinde neler olduğu hakkında olacaktır',
+      },
+      {
+        id: 5,
+        title: 'Kahvaltı',
+        price: 15,
+        image: 'assets/images/foods/scott-ish-breakfast.png',
+        description:
+          'Burda içinde neler olduğu hakkında olacaktır',
+      },
+      {
+        id: 6,
+        title: 'Köfte',
+        price: 25,
+        image: 'assets/images/foods/Kofte.png',
+        description:
+          'Burda içinde neler olduğu hakkında olacaktır',
+      },
+    ];
+  }
+
+  getFood(id: number): Food {
+    return this.getFoods().find((food) => food.id === id);
+  }
+}
